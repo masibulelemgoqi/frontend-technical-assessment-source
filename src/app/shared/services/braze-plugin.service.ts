b849f9d03db5dd5c7250039ba83e6c09811e1a31
Masibulele Mgoqi <40201744+masibulelemgoqi@users.noreply.github.com>
2025-08-06 13:16:01 +0200

    Add BrazePluginService for managing Braze content cards and events

---
+++ src/app/shared/services/braze-plugin.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { BrazeContentCard } from '@models/braze/braze-content-card';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+const BrazePlugin = require('braze-cordova-sdk');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrazePluginService {
+  private inboxNotificationSubject = new Subject<BrazeContentCard[]>();
+
+  get inboxNotifications$() {
+    return this.inboxNotificationSubject.asObservable();
+  }
+
+  initialize(token: string) {
+    BrazePlugin.setRegisteredPushToken(token);
+    BrazePlugin.subscribeToInAppMessage(true);
+    this.setUserId('default_user'); // Set a default user ID or handle it dynamically
+  }
+
+  logCustomEvent(eventName: string, properties?: Record<string, any>) {
+    if (Capacitor.isNativePlatform()) {
+      BrazePlugin.logCustomEvent(eventName, properties);
+    } else {
+      console.warn('Braze custom events are not supported on web platform');
+    }
+  }
+
+  setUserId(userId: string) {
+    BrazePlugin.updateUserId(userId);
+  }
+
+  getContentCardsFromServer() {
+    BrazePlugin.getContentCardsFromServer(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+
+  getContentCardsFromCache() {
+    BrazePlugin.getContentCardsFromCache(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+
+  logContentCardClicked(cardId: string) {
+    BrazePlugin.logContentCardClicked(cardId);
+  }
+
+  logContentCardImpression(cardId: string) {
+    BrazePlugin.logContentCardImpression(cardId);
+  }
+
+  logContentCardDismissed(cardId: string) {
+    BrazePlugin.logContentCardDismissed(cardId);
+  }
+
+  private successCallback(contentCards: BrazeContentCard[]) {
+    console.log('Content Cards fetched successfully:', JSON.stringify(contentCards));
+    const inboxCards = contentCards.filter((card) => card.extras?.type == 'inbox');
+    this.inboxNotificationSubject.next(inboxCards);
+  }
+
+  private errorCallback(error: any) {
+    console.error('Error fetching content cards:', error);
+  }
+}
